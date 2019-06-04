fc16efa00ac3663d990d10190c1bc1b54ce9936b
Cory House <housecor@gmail.com>
2019-06-04 14:36:34 -0500

    Create new app component, rename app to courses, add home

--- src/App.js
+++ src/App.js
@@ -1,76 +1,8 @@
 import React from "react";
-import * as courseApi from "./api/courseApi";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer, toast } from "react-toastify";
+import Home from "./Home";
 
-class App extends React.Component {
-  state = {
-    courses: []
-  };
-
-  componentDidMount() {
-    courseApi
-      .getCourses()
-      .then(courses => this.setState({ courses: courses }))
-      .catch(error =>
-        toast.error(
-          "🦄 Sorry, loading courses failed. Please try reloading the page. Error:" +
-            error.message
-        )
-      );
-  }
-
-  async deleteCourse(courseId) {
-    try {
-      await courseApi.deleteCourse(courseId);
-      const courses = this.state.courses.filter(
-        course => course.id !== courseId
-      );
-      this.setState({ courses: courses });
-    } catch (error) {
-      toast.error(
-        "🦄 Sorry, delete failed. Please reload and try again. Error: " +
-          error.message
-      );
-    }
-  }
-
-  renderTable() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th />
-            <th>ID</th>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.courses.map(course => (
-            <tr key={course.id}>
-              <td>
-                <button onClick={() => this.deleteCourse(course.id)}>
-                  Delete
-                </button>
-              </td>
-              <td>{course.id}</td>
-              <td>{course.title}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Courses</h1>
-        <ToastContainer />
-        <ul>{this.renderTable()}</ul>
-      </>
-    );
-  }
-}
+const App = () => {
+  return <Home />;
+};
 
 export default App;
---
+++ src/Courses.js
@@ -0,0 +1,76 @@
+import React from "react";
+import * as courseApi from "./api/courseApi";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+
+class Courses extends React.Component {
+  state = {
+    courses: []
+  };
+
+  componentDidMount() {
+    courseApi
+      .getCourses()
+      .then(courses => this.setState({ courses: courses }))
+      .catch(error =>
+        toast.error(
+          "🦄 Sorry, loading courses failed. Please try reloading the page. Error:" +
+            error.message
+        )
+      );
+  }
+
+  async deleteCourse(courseId) {
+    try {
+      await courseApi.deleteCourse(courseId);
+      const courses = this.state.courses.filter(
+        course => course.id !== courseId
+      );
+      this.setState({ courses: courses });
+    } catch (error) {
+      toast.error(
+        "🦄 Sorry, delete failed. Please reload and try again. Error: " +
+          error.message
+      );
+    }
+  }
+
+  renderTable() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th />
+            <th>ID</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.courses.map(course => (
+            <tr key={course.id}>
+              <td>
+                <button onClick={() => this.deleteCourse(course.id)}>
+                  Delete
+                </button>
+              </td>
+              <td>{course.id}</td>
+              <td>{course.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Courses</h1>
+        <ToastContainer />
+        <ul>{this.renderTable()}</ul>
+      </>
+    );
+  }
+}
+
+export default Courses;
